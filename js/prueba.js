ebfa22cf637b7ad9db1d12542e3f4ff974b6e4f2
mksh87 <keshishian.marcos@gmail.com>
2023-08-09 10:02:41 -0300

    PPT v.1 - ordering for launch

--- js/prueba.js
+++
@@ -1,2 +0,0 @@
-let b=2;
-document.write("Llamo al script desde afuera y pongo el valor "+b);
--- js/prueba2.js
+++
@@ -1,2 +0,0 @@
-let c=3;
-document.write("Llamo al script desde la misma carpeta y pongo el valor "+c);
